24565da621311b8787316da3fbc7d513289d9817
Mayur Pal <thissidemayur@gmail.com>
2025-08-16 23:41:59 +0530

    fix(ui):add dark mode text therefore it is visible in dark mode

--- app/components/GithubContributionGraph.tsx
+++ app/components/GithubContributionGraph.tsx
@@ -36,10 +36,10 @@ export default function GithubContributionGraph() {
   return (
     <Card className="relative  overflow-hidden w-full flex justify-center items-center mt-10 mb-10 ">
       <div className="text-center">
-        <CardTitle className="md:text-2xl font-semibold text-gray-800 text-2xl justify-center gap-x-2 flex items-center">
+        <CardTitle className="md:text-2xl font-semibold dark:text-slate-300 text-slate-700 text-2xl justify-center gap-x-2 flex items-center">
           <FaGithub /> GitHub Contributions – {new Date().getFullYear()}
         </CardTitle>
-        <CardDescription className="text-muted-foreground text-xs sm:text-sm  ">
+        <CardDescription className=" text-xs sm:text-sm text-neutral-600 dark:text-neutral-400 ">
           A snapshot of my coding activity, projects, open-source work, pull
           requests, and fixes this year.
         </CardDescription>
