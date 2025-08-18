fcf4ccde34fab63b61619d902628b10aeecbfb00
Mayur Pal <thissidemayur@gmail.com>
2025-08-18 20:19:45 +0530

    refactor code

--- app/components/ProjectHighlight.tsx
+++ app/components/ProjectHighlight.tsx
@@ -13,11 +13,9 @@ export default function WorkHighlight() {
 function ProjectDemo({
   title,
   description,
-  id,
 }: {
   title?: string;
   description?: string;
-  id: number;
 }) {
   const data = [
     {
@@ -107,31 +105,20 @@ function ProjectDemo({
         </div>
       ),
     },
+
     {
       id: 3,
-      title: "Calendar-Scheduling",
+      title: "Calender scheduling App",
       content: (
-        <div>
-          <p className="mb-4 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
-            Deployed 5 new components on Aceternity today
+        <div key={3}>
+          <p className="mb-8 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
+            I usually run out of copy, but when I see content this big, I try to
+            integrate lorem ipsum.
           </p>
-          <div className="mb-8">
-            <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
-              ✅ Card grid component
-            </div>
-            <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
-              ✅ Startup template Aceternity
-            </div>
-            <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
-              ✅ Random file upload lol
-            </div>
-            <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
-              ✅ Himesh Reshammiya Music CD
-            </div>
-            <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
-              ✅ Salman Bhai Fan Club registrations open
-            </div>
-          </div>
+          <p className="mb-8 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
+            Lorem ipsum is for people who are too lazy to write copy. But we are
+            not. Here are some more example of beautiful designs I built.
+          </p>
           <div className="grid grid-cols-2 gap-4">
             <Image
               src="https://assets.aceternity.com/pro/hero-sections.png"
