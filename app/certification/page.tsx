53114fc142e45e9c8d13b7d2bd37ae45e0b6b3d0
Mayur Pal <thissidemayur@gmail.com>
2025-08-18 16:23:54 +0530

    feat(ui): implement certification page and make SEO friendly

---
+++ app/certification/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Certification from "../components/Certifications";
+import SEO from "../components/SEO";
+
+export default function page() {
+  return (
+    <>
+      <SEO
+        title="Mayur Pal – Certifications & Achievements in Software, Backend, DevOps & Full-Stack"
+        description="Explore Mayur Pal’s verified certifications in Software Engineering, Backend Development, DevOps, Cloud, and Full-Stack Development. Trusted skills from Coursera, IBM, Postman, HackerRank, and more."
+        url="https://www.thissidemayur.me/certifications"
+      />
+
+      <Certification />
+    </>
+  );
+}
