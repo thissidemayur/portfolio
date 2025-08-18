012bf5eda10253ab7719af1caa0cd7c3d1473c48
Mayur Pal <thissidemayur@gmail.com>
2025-08-18 20:58:07 +0530

    push to github

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,24 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "assets.aceternity.com",
+        protocol: 'https',
+        port: '',
+        search: '',
+      }, {
+        hostname: "images.unsplash.com",
+        protocol: "https",
+      },
+      {
+        hostname: "thissidemayur.me",
+        protocol: "https",
+      }
+
+    ]
+  }
 };
 
 export default nextConfig;
