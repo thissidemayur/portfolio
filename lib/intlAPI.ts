1892980a7496f81f69b011b361f277e25a9d8c24
Mayur Pal <thissidemayur@gmail.com>
2025-08-18 18:27:29 +0530

    feat(timeFormatter): add timeformatter method

--- lib/intlAPI.ts
+++ lib/intlAPI.ts
@@ -1,9 +1,18 @@
 
 export const dateFormatter = (date: Date) => {
-    return new Intl.DateTimeFormat("en-IN", {
+    return new Intl.DateTimeFormat("en-US", {
         year: "numeric",
         month: "short",
         day: "numeric"
     }).format(date)
 
+}
+
+export const timeFormatter = (time: Date) => {
+    return new Intl.DateTimeFormat("en-US", {
+        hour12: false,
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+    }).format(time)
 }
